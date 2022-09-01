648fb9294d04ca2ddbf72eb7c3547a009f197e14
dernov01@mail.ru <StalkerOnline1998>
2022-09-01 16:25:17 +0500

    Update

--- NewsPaper/appointments/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from django.core.mail import EmailMultiAlternatives
-from datetime import datetime
-from django.template.loader import render_to_string
-from .models import Appointment
